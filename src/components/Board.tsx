52ab1aa1481ca1670d9abc6877f4375f47077235
Bartas Dausynas <bartas3@gmail.com>
2024-05-03 19:29:50 +0300

    start coding

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import style from "./Board.module.scss";
 
 const Board: React.FC = () => {
-    return (<div className={style.meh}></div>);
+    return (<div className={style.board}>
+        <div className={style.rect}>
+            <div className={style.gameArea}></div>
+        </div>
+    </div>);
 };
 
 export default Board;
